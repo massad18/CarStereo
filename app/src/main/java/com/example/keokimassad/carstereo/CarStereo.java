3fa476e75ae864969ae68f3ca9fbe6b29f0b84c3
SBEBS <ericimperio@yahoo.com>
2016-09-14 16:11:16 -0700

    PowerView

--- app/src/main/java/com/example/keokimassad/carstereo/CarStereo.java
+++ app/src/main/java/com/example/keokimassad/carstereo/CarStereo.java
@@ -12,12 +12,12 @@ public class CarStereo extends AppCompatActivity {
 
     public static Button preset1,preset2,preset3,preset4,preset5,preset6,volUp,volDown,tuneUp,tuneDown;
     public static ImageButton cdDrive,play,pause,stop,eject,forward,rewind;
-    public static ToggleButton aMfM;
+    public static ToggleButton aMfM,powerButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car_stereo);
-        ToggleButton powerButton = (ToggleButton) this.findViewById(R.id.powerButton);
+        powerButton = (ToggleButton) this.findViewById(R.id.powerButton);
         preset1 = (Button) this.findViewById(R.id.button1);
         preset2 = (Button) this.findViewById(R.id.button2);
         preset3 = (Button) this.findViewById(R.id.button3);
@@ -56,6 +56,24 @@ public class CarStereo extends AppCompatActivity {
         eject.setOnClickListener(pv);
         forward.setOnClickListener(pv);
         rewind.setOnClickListener(pv);
+        CarStereo.preset1.setEnabled(false);
+        CarStereo.preset2.setEnabled(false);
+        CarStereo.preset3.setEnabled(false);
+        CarStereo.preset4.setEnabled(false);
+        CarStereo.preset5.setEnabled(false);
+        CarStereo.preset6.setEnabled(false);
+        CarStereo.volUp.setEnabled(false);
+        CarStereo.volDown.setEnabled(false);
+        CarStereo.aMfM.setEnabled(false);
+        CarStereo.tuneUp.setEnabled(false);
+        CarStereo.tuneDown.setEnabled(false);
+        CarStereo.cdDrive.setEnabled(false);
+        CarStereo.play.setEnabled(false);
+        CarStereo.pause.setEnabled(false);
+        CarStereo.stop.setEnabled(false);
+        CarStereo.eject.setEnabled(false);
+        CarStereo.forward.setEnabled(false);
+        CarStereo.rewind.setEnabled(false);
 
     }
 }
